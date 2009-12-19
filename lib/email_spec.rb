55a459339235f6ea6774fb6a3d19f063caf976ed
John Mendonca <rangerfrex@hotmail.com>
2009-12-19 12:09:32 -0800

    merge deliveries methods into helpers

--- lib/email_spec.rb
+++ lib/email_spec.rb
@@ -4,6 +4,5 @@ end
 
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'email_spec/deliveries'
 require 'email_spec/helpers'
 require 'email_spec/matchers'
--- lib/email_spec/helpers.rb
+++ lib/email_spec/helpers.rb
@@ -1,9 +1,43 @@
 require 'uri'
-require 'email_spec/deliveries'
 
 module EmailSpec
+
+  if defined?(Pony)
+    #reopen Pony module and replace mail method
+    module ::Pony
+      def self.deliveries
+        @deliveries ||= []
+      end
+
+      def self.mail(options)
+        deliveries << build_tmail(options)
+      end
+    end
+  end
+
   module Helpers
-    include Deliveries
+    if defined?(Pony)
+      def mailer; Pony; end
+    else
+      #no mail sender available
+      def mailer; raise("email_spec requires Pony"); end
+    end
+
+    def all_emails
+      mailer.deliveries
+    end
+
+    def last_email_sent
+      mailer.deliveries.last || raise("No email has been sent!")
+    end
+
+    def reset_mailer
+      mailer.deliveries.clear
+    end
+
+    def mailbox_for(address)
+      mailer.deliveries.select { |m| m.to.include?(address) || (m.bcc && m.bcc.include?(address)) || (m.cc && m.cc.include?(address)) }
+    end
 
     def visit_in_email(link_text)
       visit(parse_email_for_link(current_email, link_text))
