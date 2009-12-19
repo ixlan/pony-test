956d82742661ea4618f3586258a65263284cea5f
John Mendonca <rangerfrex@hotmail.com>
2009-12-18 18:55:36 -0800

    improved readability of matchers.rb

--- lib/email_spec/matchers.rb
+++ lib/email_spec/matchers.rb
@@ -1,12 +1,10 @@
 module EmailSpec
-
-    module Matchers
+  module Matchers
 
     class DeliverTo
-
-      def initialize(expected_email_addresses_or_objects_that_respond_to_email)
-        emails = expected_email_addresses_or_objects_that_respond_to_email.map do |email_or_object|
-          email_or_object.kind_of?(String) ? email_or_object : email_or_object.email
+      def initialize(recipients)
+        emails = recipients.map do |recipient|
+          recipient.kind_of?(String) ? recipient : recipient.email
         end
 
         @expected_email_addresses = emails.sort
@@ -23,7 +21,8 @@ module EmailSpec
       end
 
       def failure_message
-        "expected #{@email.inspect} to deliver to #{@expected_email_addresses.inspect}, but it delivered to #{@actual_recipients.inspect}"
+        "expected #{@email.inspect} to deliver to #{@expected_email_addresses.inspect}, " + 
+          "but it delivered to #{@actual_recipients.inspect}"
       end
 
       def negative_failure_message
@@ -31,14 +30,13 @@ module EmailSpec
       end
     end
 
-    def deliver_to(*expected_email_addresses_or_objects_that_respond_to_email)
-      DeliverTo.new(expected_email_addresses_or_objects_that_respond_to_email.flatten)
+    def deliver_to(*recipients)
+      DeliverTo.new(recipients.flatten)
     end
 
     alias :be_delivered_to :deliver_to
 
     class DeliverFrom
-
       def initialize(email)
         @expected_email_addresses = email
       end
@@ -54,7 +52,8 @@ module EmailSpec
       end
 
       def failure_message
-        "expected #{@email.inspect} to deliver from #{@expected_email_addresses.inspect}, but it delivered from #{@actual_sender.inspect}"
+        "expected #{@email.inspect} to deliver from #{@expected_email_addresses.inspect}, " + 
+          "but it delivered from #{@actual_sender.inspect}"
       end
 
       def negative_failure_message
@@ -69,10 +68,9 @@ module EmailSpec
     alias :be_delivered_from :deliver_from
 
     class BccTo
-
-      def initialize(expected_email_addresses_or_objects_that_respond_to_email)
-        emails = expected_email_addresses_or_objects_that_respond_to_email.map do |email_or_object|
-          email_or_object.kind_of?(String) ? email_or_object : email_or_object.email
+      def initialize(recipients)
+        emails = recipients.map do |recipient|
+          recipient.kind_of?(String) ? recipient : recipient.email
         end
 
         @expected_email_addresses = emails.sort
@@ -89,7 +87,8 @@ module EmailSpec
       end
 
       def failure_message
-        "expected #{@email.inspect} to bcc to #{@expected_email_addresses.inspect}, but it was bcc'd to #{@actual_recipients.inspect}"
+        "expected #{@email.inspect} to bcc to #{@expected_email_addresses.inspect}, " + 
+          "but it was bcc'd to #{@actual_recipients.inspect}"
       end
 
       def negative_failure_message
@@ -97,8 +96,8 @@ module EmailSpec
       end
     end
 
-    def bcc_to(*expected_email_addresses_or_objects_that_respond_to_email)
-      BccTo.new(expected_email_addresses_or_objects_that_respond_to_email.flatten)
+    def bcc_to(*recipient)
+      BccTo.new(recipient.flatten)
     end
 
     def have_subject(expected)
@@ -113,8 +112,10 @@ module EmailSpec
           given_subject == expected
         else
           matcher.description = "have subject matching #{expected.inspect}"
-          matcher.failure_message = "expected the subject to match #{expected.inspect}, but did not.  Actual subject was: #{given_subject.inspect}"
-          matcher.negative_failure_message = "expected the subject not to match #{expected.inspect} but #{given_subject.inspect} does match it."
+          matcher.failure_message = "expected the subject to match #{expected.inspect}, " + 
+            "but did not.  Actual subject was: #{given_subject.inspect}"
+          matcher.negative_failure_message = "expected the subject not to match #{expected.inspect} " + 
+            "but #{given_subject.inspect} does match it."
 
           !!(given_subject =~ expected)
         end
@@ -126,14 +127,18 @@ module EmailSpec
 
          if expected.is_a?(String)
            matcher.description = "include email with subject of #{expected.inspect}"
-           matcher.failure_message = "expected at least one email to have the subject #{expected.inspect} but none did. Subjects were #{given_emails.map(&:subject).inspect}"
-           matcher.negative_failure_message = "expected no email with the subject #{expected.inspect} but found at least one. Subjects were #{given_emails.map(&:subject).inspect}"
+           matcher.failure_message = "expected at least one email to have the subject #{expected.inspect} " + 
+             "but none did. Subjects were #{given_emails.map(&:subject).inspect}"
+           matcher.negative_failure_message = "expected no email with the subject #{expected.inspect} " + 
+             "but found at least one. Subjects were #{given_emails.map(&:subject).inspect}"
 
            given_emails.map(&:subject).include?(expected)
          else
            matcher.description = "include email with subject matching #{expected.inspect}"
-           matcher.failure_message = "expected at least one email to have a subject matching #{expected.inspect}, but none did. Subjects were #{given_emails.map(&:subject).inspect}"
-           matcher.negative_failure_message = "expected no email to have a subject matching #{expected.inspect} but found at least one. Subjects were #{given_emails.map(&:subject).inspect}"
+           matcher.failure_message = "expected at least one email to have a subject matching #{expected.inspect}, " + 
+             "but none did. Subjects were #{given_emails.map(&:subject).inspect}"
+           matcher.negative_failure_message = "expected no email to have a subject matching #{expected.inspect} " + 
+             "but found at least one. Subjects were #{given_emails.map(&:subject).inspect}"
 
            !!(given_emails.any?{ |mail| mail.subject =~ expected })
          end
@@ -147,15 +152,19 @@ module EmailSpec
            normalized_body = given.body.gsub(/\s+/, " ")
            normalized_expected = expected.gsub(/\s+/, " ")
            matcher.description = "have body including #{normalized_expected.inspect}"
-           matcher.failure_message = "expected the body to contain #{normalized_expected.inspect} but was #{normalized_body.inspect}"
-           matcher.negative_failure_message = "expected the body not to contain #{normalized_expected.inspect} but was #{normalized_body.inspect}"
+           matcher.failure_message = "expected the body to contain #{normalized_expected.inspect} " +
+             "but was #{normalized_body.inspect}"
+           matcher.negative_failure_message = "expected the body not to contain #{normalized_expected.inspect} " + 
+             "but was #{normalized_body.inspect}"
 
            normalized_body.include?(normalized_expected)
          else
            given_body = given.body
            matcher.description = "have body matching #{expected.inspect}"
-           matcher.failure_message = "expected the body to match #{expected.inspect}, but did not.  Actual body was: #{given_body.inspect}"
-           matcher.negative_failure_message = "expected the body not to match #{expected.inspect} but #{given_body.inspect} does match it."
+           matcher.failure_message = "expected the body to match #{expected.inspect}, " + 
+             "but did not.  Actual body was: #{given_body.inspect}"
+           matcher.negative_failure_message = "expected the body not to match #{expected.inspect} " + 
+             "but #{given_body.inspect} does match it."
 
            !!(given_body =~ expected)
          end
@@ -168,19 +177,22 @@ module EmailSpec
 
           if expected_value.is_a?(String)
             matcher.description = "have header #{expected_name}: #{expected_value}"
-            matcher.failure_message = "expected the headers to include '#{expected_name}: #{expected_value}' but they were #{given_header.inspect}"
-            matcher.negative_failure_message = "expected the headers not to include '#{expected_name}: #{expected_value}' but they were #{given_header.inspect}"
+            matcher.failure_message = "expected the headers to include '#{expected_name}: #{expected_value}' " + 
+            "but they were #{given_header.inspect}"
+            matcher.negative_failure_message = "expected the headers not to include '#{expected_name}: #{expected_value}' " +
+            "but they were #{given_header.inspect}"
 
             given_header[expected_name].to_s == expected_value
           else
             matcher.description = "have header #{expected_name} with value matching #{expected_value.inspect}"
-            matcher.failure_message = "expected the headers to include '#{expected_name}' with a value matching #{expected_value.inspect} but they were #{given_header.inspect}"
-            matcher.negative_failure_message = "expected the headers not to include '#{expected_name}' with a value matching #{expected_value.inspect} but they were #{given_header.inspect}"
+            matcher.failure_message = "expected the headers to include '#{expected_name}' " + 
+              "with a value matching #{expected_value.inspect} but they were #{given_header.inspect}"
+            matcher.negative_failure_message = "expected the headers not to include '#{expected_name}' " + 
+              "with a value matching #{expected_value.inspect} but they were #{given_header.inspect}"
 
             given_header[expected_name].to_s =~ expected_value
           end
         end
       end
-
   end
 end
