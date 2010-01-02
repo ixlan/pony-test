98aee31595b355f147c0b4752edf5a82ccea34bc
John Mendonca <rangerfrex@hotmail.com>
2010-01-01 22:25:59 -0800

    writing spec for helpers

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,8 +1,342 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
-describe PonyTest::Helpers do
-  before(:each) do
-    reset_mailer
+describe Pony::TestStruct do
+  include Pony::TestStruct
+
+  def email_matches(tmail, hash)
+    tmail2 = Pony.build_tmail(hash)
+    tmail.to == tmail2.to &&
+      tmail.from == tmail2.from &&
+      tmail.cc == tmail2.cc &&
+      tmail.bcc == tmail2.bcc &&
+      tmail.subject == tmail2.subject &&
+      tmail.body == tmail2.body
   end
 
+  EMAIL_1 = { :to => 'foo@example.com', :from => 'bar@example.com', :subject => 'Hello there', :body => 'Hi' }
+  EMAIL_2 = { :to => 'bob@example.com', :from => 'neb@example.com', :subject => 'Sup buddy', :body => 'Geez' }
+  EMAIL_3 = { :to => 'rob@example.com', :from => 'nib@example.com', :subject => 'Come on already', :body => 'Man' }
+  EMAIL_CC = { :to => 'rob@example.com', :cc => 'foo@example.com', :from => 'nib@example.com', :subject => 'Come on already', :body => 'Geez' }
+  EMAIL_BCC = { :to => 'neb@example.com', :bcc => 'foo@example.com', :from => 'nib@example.com', :subject => 'Hello joe', :body => 'Man' }
+  
+  LINKS = ['http://example.com/foobar', 'https://www.example.com' 'http://example.com/food/']
+  EMAIL_LINKS_1 = { :to => 'foo@example.com', :from => 'bar@example.com', :subject => 'Coffee', :body => 'Hi ' + LINKS[0] + ' ' + LINKS[1] }
+  EMAIL_LINKS_2 = { :to => 'bob@example.com', :from => 'neb@example.com', :subject => 'The goods', :body => LINKS[2] }
+
+  before(:each) do
+    reset_mailer
+    deliveries.empty?.should == true
+  end
+
+  describe 'deliveries' do
+    it 'should capture Pony mail deliveries' do
+      Pony.mail(EMAIL_1)
+      email = deliveries.last
+      email_matches(email, EMAIL_1).should == true
+    end
+  end
+
+  describe 'current_email' do
+    it 'should return the last email sent by default' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = current_email
+      email_matches(email, EMAIL_3).should == true
+      email_matches(email, EMAIL_1).should == false
+      email_matches(email, EMAIL_2).should == false
+    end
+
+    it 'should raise an error when no email' do
+      current_email.should raise_error
+    end
+  end
+  
+  describe 'reset_mailer' do
+    it 'should clear deliveries, current_email, and current_email_address' do
+      Pony.mail(EMAIL_1)
+      self.current_email_address = 'foo@example.com'
+      deliveries.length.should == 1
+      current_email.should_not == nil
+      current_email_address.should_not == nil
+      reset_mailer
+      deliveries.empty?.should == true
+      current_email.should == nil
+      current_email_address.should == nil
+    end
+  end
+
+  describe 'last_email_sent' do
+    it 'should return the last email sent' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = last_email_sent
+      email_matches(email, EMAIL_3).should == true
+      email_matches(email, EMAIL_1).should == false
+      email_matches(email, EMAIL_2).should == false
+    end
+
+    it 'should set current email to the last email sent' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      #change current_email to something else first
+      open_email
+      email_matches(current_email, EMAIL_1)
+      last_email_sent
+      email_matches(current_email, EMAIL_3)
+    end
+
+    it 'should raise an error when no email' do
+      last_email_sent.should raise_error
+    end
+  end
+
+  describe 'inbox_for' do
+    it 'should return only emails for one email address' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      emails = inbox_for('foo@example.com')
+      emails.length.should == 3
+      email_matches(emails[0], EMAIL_1).should == true
+      email_matches(emails[1], EMAIL_CC).should == true
+      email_matches(emails[2], EMAIL_BCC).should == true
+    end
+
+    it 'should return all emails when no address specified' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      emails = inbox
+      emails.length.should == 5
+    end
+
+    it 'should set current_email_address' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      emails = inbox_for('foo@example.com')
+      current_email_address.should == 'foo@example.com'
+    end
+
+    it 'should use current_email_address by default' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      #set current address first
+      open_email_for('foo@example.com')
+      current_email_address.should == 'foo@example.com'
+      emails = inbox
+      emails.length.should == 3
+      email_matches(emails[0], EMAIL_1).should == true
+      email_matches(emails[1], EMAIL_CC).should == true
+      email_matches(emails[2], EMAIL_BCC).should == true
+    end
+
+    it 'should not raise an error when there is no email' do
+      inbox
+    end
+  end
+
+  describe 'open_email_for' do
+    it 'should return the first matching email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email_for('bob@example.com')
+      email_matches(email, EMAIL_2).should == true
+    end
+
+    it 'should find an email by subject' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email(:with_subject => 'already')
+      email_matches(email, EMAIL_3).should == true
+    end
+
+    it 'should find an email by body' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email(:with_body => 'Geez')
+      email_matches(email, EMAIL_2).should == true
+    end
+
+    it 'should set current_email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email(:with_body => 'Geez')
+      email_matches(email, EMAIL_2).should == true
+      email_matches(current_email, EMAIL_2).should == true
+    end
+
+    it 'should set current_email_address' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email_for('bob@example.com')
+      email_matches(email, EMAIL_2).should == true
+      current_email_address.should == 'bob@example.com'
+    end
+
+    it 'should use current_email_address by default' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      #set current address to something else
+      inbox_for('foo@example.com')
+      current_email_address.should == 'foo@example.com'
+      email = open_email_for('bob@example.com')
+      email_matches(email, EMAIL_2).should == true
+      current_email_address.should == 'bob@example.com'
+    end
+
+    it 'should return the first sent email when address not specified' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email
+      email_matches(email, EMAIL_1).should == true
+      current_email_address.should == nil
+    end
+
+    it 'should raise an error when no matching email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      open_email(:with_subject => 'viagra').should raise_error
+    end
+
+    it 'should raise an error when there is no email' do
+      open_email.should raise_error
+    end
+  end
+
+  describe 'find_email_for' do
+    it 'should return all matching email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      email = find_email_for('neb@example.com')
+      email.length.should == 2
+      email_matches(emails[0], EMAIL_2).should == true
+      email_matches(emails[1], EMAIL_BCC).should == true
+    end
+
+    it 'should find email by title' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      email = find_email(:with_subject => 'Hello')
+      email.length.should == 2
+      email_matches(emails[0], EMAIL_1).should == true
+      email_matches(emails[1], EMAIL_BCC).should == true
+    end
+
+    it 'should find email by body' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      email = find_email(:with_subject => 'Man')
+      email.length.should == 2
+      email_matches(emails[0], EMAIL_3).should == true
+      email_matches(emails[1], EMAIL_BCC).should == true
+    end
+
+    it 'should not set current_email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      email = find_email(:with_subject => 'Man')
+      current_email.should == nil
+    end
+
+    it 'should set current_email_address' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      #set current address to something else
+      inbox_for('foo@example.com')
+      current_email_address.should == 'foo@example.com'
+      email = find_email_for('neb@example.com')
+      current_email_address.should == 'neb@example.com'
+    end
+
+    it 'should use current_email_address by default' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      open_email_for('foo@example.com')
+      email = find_email
+      email.length.should == 3
+      email_matches(emails[0], EMAIL_1).should == true
+      email_matches(emails[1], EMAIL_CC).should == true
+      email_matches(emails[2], EMAIL_BCC).should == true
+    end
+
+    it 'should return all email when address not specified' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      email = find_email
+      email.length.should == 5
+    end
+
+    it 'should raise an error when no matching email' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      Pony.mail(EMAIL_CC) && Pony.mail(EMAIL_BCC)
+      find_email(:with_body => 'gold chains').should raise_error
+    end
+
+    it 'should raise an error when there is no email' do
+      find_email.should raise_error
+    end
+  end
+
+  describe 'email_links' do
+    it 'should return all links in current_email' do
+      Pony.mail(EMAIL_LINKS_1)
+      links = email_links
+      links.length.should == 2
+      links[0].should == LINKS[0]
+      links[1].should == LINKS[1]
+    end
+
+    it 'should return all links in given email' do
+      Pony.mail(EMAIL_LINKS_1) && Pony.mail(EMAIL_LINKS_2)
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      email = open_email(:with_subject => 'goods')
+      #to ensure it is not just reading current_email
+      open_email(:with_subject => 'Coffee')
+      links = email_links(email)
+      links.length.should == 1
+      links[0].should == LINKS[2]
+    end
+
+    it 'should raise an error when no links' do
+      Pony.mail(EMAIL_1) && Pony.mail(EMAIL_2) && Pony.mail(EMAIL_3)
+      open_email
+      current_email.should_not == nil
+      email_links.should raise_error
+    end
+
+    it 'should raise an error when no email specified, nor current_email' do
+      email_links.should raise_error
+    end
+  end
+
+  describe 'email_links_matching' do
+    it 'should return all matching links in current_email' do
+      Pony.mail(EMAIL_LINKS_1) && Pony.mail(EMAIL_LINKS_2)
+      open_email
+      links = email_links_matching('foobar') 
+      links.length.should == 1
+      links[0].should == LINKS[0]
+    end
+
+    it 'should return all matching links in an email' do
+      Pony.mail(EMAIL_LINKS_1) && Pony.mail(EMAIL_LINKS_2)
+      email = open_email                            
+      #to ensure it is not just reading current_email
+      last_email_sent
+      links = email_links_matching('foobar', email) 
+      links.length.should == 1
+      links[0].should == LINKS[0]
+    end
+
+    it 'should raise an error when no matching links' do
+      Pony.mail(EMAIL_LINKS_1) && Pony.mail(EMAIL_LINKS_2)
+      email_links_matching('food').should_not raise_error
+      email_links_matching('wood').should raise_error
+    end
+  end
+
+  describe 'translate_address' do
+    it 'should return appropriate values' do
+      translate_address('I').should == nil
+      translate_address('they').should == nil
+      translate_address('fakeaddress').should == 'fakeaddress'
+    end
+  end
+
+  describe 'translate_email_count' do
+    it 'should return appropriate values' do
+      translate_email_count('no').should == 0
+      translate_email_count('a').should == 1
+      translate_email_count('an').should == 1
+      translate_email_count('99').should == 99
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require 'rubygems'
 require 'spec'
+require 'pony'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/pony_test.rb')
